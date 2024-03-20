df9e44054d7f76c7da9d1b33d2b0e2e10655b984
NikoShivkumar <159164670+NikoShivkumar@users.noreply.github.com>
2024-03-20 15:02:53 +0000

    u

--- js/script.js
+++ js/script.js
@@ -8,10 +8,10 @@ function enterClicked () {
   // this function gets user's first name and age and displays it back to user
 
   // get first name from text field
-  let StreetNumber = document.getElementById("StreetNumber").value
+  let StreetNumber = parseInt(document.getElementById("StreetNumber").value)
 	// get age from text field and cast it to integer
-  let StreetName = parseInt(document.getElementById("StreetName").value)
+  let StreetName = document.getElementById("StreetName").value
 
   // display name and age back to user
-  document.getElementById('user-info').innerHTML = "Your Street address is " + StreetNumber + StreetName + "."
+  document.getElementById('user-info').innerHTML = "Your Street address is " + StreetNumber + " " + StreetName + "."
 }
